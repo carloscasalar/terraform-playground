c7ec74f6b136a764d194ab35ed154a6ef0f5a5b9
Carlos Castillo <castillo.st@gmail.com>
2026-02-23 19:40:24 +0000

    Uncomment aws_instance resource for NGINX setup; update instance type to t3.micro

--- main.tf
+++ main.tf
@@ -70,26 +70,26 @@ resource "aws_security_group" "nginx-sg" {
 }
 
 # INSTANCES #
-# resource "aws_instance" "nginx1" {
-#   ami                    = nonsensitive(data.aws_ssm_parameter.ami.value)
-#   instance_type          = "t2.micro"
-#   subnet_id              = aws_subnet.subnet1.id
-#   vpc_security_group_ids = [aws_security_group.nginx-sg.id]
-#
-#   user_data = <<EOF
-# #!/bin/bash
-# # Update the system
-# dnf update -y
-#
-# # Install NGINX
-# dnf install nginx -y
-#
-# # Start NGINX service
-# systemctl start nginx
-#
-# # Enable NGINX to start on boot
-# systemctl enable nginx
-# sudo rm /usr/share/nginx/html/index.html
-# echo '<html><head><title>Lemon Land Server</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">You did it! Have a &#127790;</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html
-#   EOF
-# }
+resource "aws_instance" "nginx1" {
+  ami                    = nonsensitive(data.aws_ssm_parameter.ami.value)
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.subnet1.id
+  vpc_security_group_ids = [aws_security_group.nginx-sg.id]
+
+  user_data = <<EOF
+#!/bin/bash
+# Update the system
+dnf update -y
+
+# Install NGINX
+dnf install nginx -y
+
+# Start NGINX service
+systemctl start nginx
+
+# Enable NGINX to start on boot
+systemctl enable nginx
+sudo rm /usr/share/nginx/html/index.html
+echo '<html><head><title>Lemon Land Server</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">You did it! Have a &#127790;</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html
+  EOF
+}
