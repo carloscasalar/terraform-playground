1add025766577c63234700d6b031660fcb788b67
Carlos Castillo <castillo.st@gmail.com>
2026-03-01 13:20:22 +0000

    Add outputs for NGINX instances and load balancer DNS name

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,10 @@
-output "aws_instance_public_dns" {
+output "aws_instance_public_dns_nginx1" {
   value = aws_instance.nginx1.public_dns
+}
+output "aws_instance_public_dns_nginx2" {
+  value = aws_instance.nginx2.public_dns
+}
+output "aws_lb_dns_name" {
+  value       = aws_lb.nginx_lb.dns_name
+  description = "The DNS name of the load balancer"
 }
